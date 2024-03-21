a0c6d5899a3ea6e30ad2e0e3767e5364f9a4a902
EndyB <endyb.urbanista@gmail.com>
2024-03-21 22:17:10 +1100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI()
 
 @app.get("/")
 def home():
     return {"Greeting": "Hello World from FastAPI"}
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, port=8080, host='0.0.0.0')
