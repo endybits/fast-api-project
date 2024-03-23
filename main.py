d5ed09a92dd4404eb600d9eb7039bba37f79cb9f
EndyB <endyb.urbanista@gmail.com>
2024-03-23 13:55:18 +1100

    Update main.py

--- main.py
+++ main.py
@@ -8,5 +8,11 @@ def home():
     return {"Greeting": "Hello World from FastAPI"}
 
 
+# Health check endpoint
+@app.get('/health')
+async def healthcheck():
+    return Response(content="OK", status_code=status.HTTP_200_OK)
+
+
 if __name__ == '__main__':
     uvicorn.run(app, port=8080, host='0.0.0.0')
